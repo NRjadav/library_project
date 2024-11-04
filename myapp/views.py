0ab9800495f295632814bf1aaafca9af08fdf8bc
GohilVidhi <gohilvidhi3@gmail.com>
2024-11-04 17:48:37 +0530

    ok

--- myapp/views.py
+++ myapp/views.py
@@ -5,7 +5,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 # Create your views here.
 def home(request):
-    return HttpResponse("hello")
+    return HttpResponse("hello1")
 
 
 #---------------Pool View----------------------        
